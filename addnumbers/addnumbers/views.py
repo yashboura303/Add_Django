c2c744bd8b6cf5d497a0a870295eda298cfd42a0
Yash Boura <yashboura303@gmail.com>
2019-06-01 22:25:52 +0530

    Initial commit

---
+++ addnumbers/addnumbers/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+def home(request):
+	return render(request,'home.html')
+
+def add(request):
+	number1 = int(request.GET["num1"])
+	number2 = int(request.GET["num2"])
+
+	addition = number1 + number2
+
+	return render(request, 'add.html',{'addition': addition})
